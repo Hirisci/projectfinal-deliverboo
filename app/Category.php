a34ea095ee08f2155e76034f6c21cbcf53be5786
Giaki22 <giaki22@hotmail.com>
2022-08-26 11:57:06 +0200

    Add Slug Creator

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,35 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $guarded =['slug'];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom(['name', 'id'])
+            ->saveSlugsTo('slug');
+    }
+
     public function categories(){
         return $this->belongsToMany('App\User');
+    }
+
+        /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
