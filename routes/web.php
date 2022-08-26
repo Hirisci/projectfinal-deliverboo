a3712deecb223d13407497746ffcf3cfd6ea6a06
Hirisci <www.alan92@icloud.com>
2022-08-26 15:56:22 +0200

    back office

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::middleware('auth')
+    ->namespace('Admin') //tutti i controller saranno dentro al namespace Admin
+    ->name('admin.') //nome della rotta
+    ->prefix('admin') //prefisso rotta
+    ->group(function() { // creo gruppo che richiama /admin/home che richiama il controller
+        Route::get('/home', 'HomeController@index')->name('home');
+});
+
+
+// Route::get(
+//     "{any?}",
+//     function(){
+//         return view('guest.home');}
+// )->where("any",".*");
