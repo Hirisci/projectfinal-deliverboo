5a02857800ea0e9094a6dde50721ea8adf4f9e01
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-08-27 10:46:54 +0200

    first view for create new category

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h1>Creazione Categoria Ristorante</h1>
+            </div>
+            <div class="card-body"></div>
+        </div>
+    </div>
+@endsection
