1fb3fad0448f18d430edaf348c31182d9a1cf253
Giaki22 <giaki22@hotmail.com>
2022-08-27 10:55:42 +0200

    + Create Blade

--- resources/views/admin/plate/create.blade.php
+++ resources/views/admin/plate/create.blade.php
@@ -1,12 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="card">
-            <div class="card-header">
-                <h1>Creazione piatto</h1>
-            </div>
-            <div class="card-body"></div>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h1>Creazione Piatto</h1>
+        </div>
+        <div class="card-body">
+            <form action="{{route('admin.plate.store')}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Nome Piatto</label>
+                    <input type="text" class="form-control" id="name" name="name">
+                </div>
+                <div class="form-group">
+                    <label for="title">Descrizione Piatto</label>
+                    <textarea name="description" id="description" class="form-control" cols="30" rows="5"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="title">Prezzo</label>
+                    <textarea name="price" id="price" class="form-control" cols="30" rows="5"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Crea</button>
+              </form>
         </div>
     </div>
+</div>
 @endsection
