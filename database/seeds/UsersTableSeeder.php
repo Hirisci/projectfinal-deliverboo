9d88a0b237cfa5b04e0f491a90fce5a2faa53c59
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-08-29 12:40:02 +0200

    aggiunto primo ristorante a DB (tutto worka)

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -30,9 +30,9 @@ class UsersTableSeeder extends Seeder
                 "address" => "Passaggio Duomo, 2, 20123 Milano MI",
                 "img" => "https://canaledieci.it/wp-content/uploads/2022/02/MC-CAFE-MC-DONALDS-ASSUNZIONI.jpg",
                 "logo" => "https://i.pinimg.com/originals/3e/cb/a8/3ecba88bff841698cef03d71220adaf6.jpg",
-                "mail" => "test1@mail.com",
+                "email" => "test1@mail.com",
                 "password" => "$2y$10\$vPsht5Ye8dWukO9K4qyx3.6KXfGxjQ0S9MGP9zjA0.k.c2tJJItXW",
-                "vat" => "12345678910"
+                "vat" => "1234"
             ],
         ];
 
@@ -41,9 +41,10 @@ class UsersTableSeeder extends Seeder
             $newUser->name = $restaurant['name'];
             $newUser->address = $restaurant['address'];
             $newUser->img = $restaurant['img'];
-            $newUser->mail = $restaurant['mail'];
+            $newUser->email = $restaurant['email'];
             $newUser->password = $restaurant['password'];
             $newUser->vat = $restaurant['vat'];
+            $newUser->save();
         }
     }
 }
