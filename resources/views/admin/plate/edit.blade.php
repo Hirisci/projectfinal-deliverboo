892dfc928bb0f434790e7810a969cd0a4fd5cb37
Giaki22 <giaki22@hotmail.com>
2022-08-29 14:34:25 +0200

    + MemoryForm - Bug on checked box "is_visible" in Edit() PlateController

--- resources/views/admin/plate/edit.blade.php
+++ resources/views/admin/plate/edit.blade.php
@@ -7,15 +7,18 @@
                 <h1>Modifica Piatto:{{$plate->name}}</h1>
             </div>
             <div class="card-body">
-                <form action="{{route('admin.plate.update', $plate->id)}}" method="POST">
+                <form action="{{route('admin.plate.update', $plate)}}" method="POST">
                     @csrf
                     @method('PUT')
                     <div class="form-group">
                         <label for="name">Nome Piatto</label>
                         <input type="text" class="form-control" id="name" name="name" value="{{old('name', $plate->name)}}">
-                        @error('name')
-                            <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
+                        <label for="description">Descrizione Piatto</label>
+                        <input type="text" class="form-control" id="description" name="description" value="{{old('description', $plate->description)}}">
+                        <label for="price">Prezzo</label>
+                        <input type="number" class="form-control" id="price" name="price" value="{{old('price', $plate->price)}}">
+                        <label class="form-check-label" for="is_visible">Pubblica</label>
+                        <input type="checkbox" class="form-check-input @error('is_visible') is-invalid @enderror" id="is_visible" name="is_visible" {{old('is_visible', $plate->is_visible) ? 'checked' : ''}}>
                     </div>
                     <button type="submit" class="btn btn-warning">Modifica</button>
                 </form>
