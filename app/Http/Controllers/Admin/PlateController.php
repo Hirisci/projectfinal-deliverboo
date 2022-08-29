d1653327a0b531023c6f6e6db43eb3b6f1549d49
Giaki22 <giaki22@hotmail.com>
2022-08-29 10:13:37 +0200

    + Index PlateController + Create PlateController

--- app/Http/Controllers/Admin/PlateController.php
+++ app/Http/Controllers/Admin/PlateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Plate;
 use Illuminate\Http\Request;
 
 class PlateController extends Controller
@@ -14,7 +15,8 @@ class PlateController extends Controller
      */
     public function index()
     {
-        //
+        $plates = Plate::all();
+        return view('admin.plate.index', compact('plates'));
     }
 
     /**
@@ -35,7 +37,20 @@ class PlateController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validazione dati
+        $request->validate([
+            'name' => 'required | string | max:50',
+            'description' => 'required | string | max:200',
+            'price' => 'required | numeric | max:5'
+        ]);
+        //prendo i dati dal request e creo la nuova categoria
+        $data = $request->all();
+        $newPlate = new Plate();
+        $newPlate->fill($data);
+        $newPlate->slug = $this->getSlug($data['name']);
+        $newPlate->save();
+        //reindirizzo a un altra pagina
+        return redirect()->route('admin.plate.index');
     }
 
     /**
---
+++ resources/views/admin/plate/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h1>Lista Piatti</h1>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Azioni</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <div class="div mb-3">
+                        <a href="{{route('admin.plate.create')}}" type="button" class="btn btn-success">Crea nuovo piatto</a>
+                    </div>
+                    @foreach ($plates as $plate)
+                        <tr>
+                            <td>{{$plate->id}}</td>
+                            <td>{{$plate->name}}</td>
+                            {{-- azioni --}}
+                            <td>
+                                <a href="{{route('admin.plate.show', $plate->id)}}" class="btn btn-primary">Visualizza</a>
+                                <a href="{{route('admin.plate.edit', $plate->id)}}" class="btn btn-warning">Modifica</a>
+                                <form action="{{route('admin.plate.destroy' , $plate->id )}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Elimina</button>
+                                </form>
+                            </td> 
+                        </tr>
+                    @endforeach
+                </tbody>
+              </table>
+        </div>
+    </div>
+</div>
+@endsection
