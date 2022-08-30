091b6aa28886b194402db15af201f74f241e550f
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-08-30 09:51:51 +0200

    fix ordinamento Riepilogo Totale

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -7,18 +7,18 @@
             <h1>Riepilogo Locale</h1>
         </div>
         <div class="row">
-            <div class="name col-2">nome locale</div>
-            <div class="name col-2">slug</div>
-            <div class="name col-2">id</div>
+            <div class="name col-1">ID</div>
+            <div class="name col-2">Nome Locale</div>
+            <div class="name col-4">Indirizzo</div>
+            <div class="name col-2">Slug</div>
             <div class="name col-2">partita IVA</div>
-            <div class="name col-2">address</div>
         </div>
         
         <div class="row">
-            <div class="name col-2">{{$user->address}}</div>
+            <div class="name col-1">{{$user->id}}</div>
             <div class="name col-2">{{$user->name}}</div>
+            <div class="name col-4">{{$user->address}}</div>
             <div class="name col-2">{{$user->slug}}</div>
-            <div class="name col-2">{{$user->id}}</div>
             <div class="name col-2">{{$user->vat}}</div>
         </div>
 
