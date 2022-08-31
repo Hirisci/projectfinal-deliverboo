0b538d198633f9e75ec1532231f8bf6c7aff02e8
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-08-31 16:51:29 +0200

    styling dashboard sx

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,20 +4,33 @@
 <div class="container menu">
     
     <div class="menu-sx">
-        <ul>
-            <li class="section-title">Account</li>
-            <ul>
+        <ul class="py-4">
+            <div class="section-title-container">
+                <div class="section-title px-3"><h6>ACCOUNT</h6></div>
+                <div class="triangle-element"></div>
+            </div>
+            <ul class="py-3 mx-3">
                 <li><a href="{{route('admin.restaurant.index')}}">Visualizza ristorante</a></li>
                 <li><a href="{{route('admin.restaurant.edit', Auth::user())}}">Modifica dati ristorante</a></li>
             </ul>
             
-            <li>Piatti</li>
-            <ul>
+            <div class="section-title-container">
+                <div class="section-title px-3"><h6>PIATTI</h6></div>
+                <div class="triangle-element"></div>
+            </div>
+            
+            <ul class="py-3 mx-3">
                 <li><a href="{{route('admin.plate.index')}}">Visualizza piatti</a></li>
                 <li><a href="{{route('admin.plate.create')}}">Aggiungi piatti</a></li>
             </ul>
-            <li>Statistiche vendita</li>
-            <li><a href="{{route('admin.order.index')}}">visualizza Ordini</a></li>
+            <div class="section-title-container">
+                <div class="section-title px-3"><h6>STATISCHE VENDITA</h6></div>
+                <div class="triangle-element"></div>
+            </div>
+            <ul class="py-3 mx-3">
+                <li><a href="{{route('admin.order.index')}}">Visualizza ordini</a></li>
+            </ul>
+            
         </ul>
     </div>
     <div class="menu-dx">
