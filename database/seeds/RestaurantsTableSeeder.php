4136604d5c5ad26ebe76e4f2748172144020a463
Hirisci <www.alan92@icloud.com>
2022-09-01 11:12:27 +0200

    change img link in seederRestoaurant

--- database/seeds/RestaurantsTableSeeder.php
+++ database/seeds/RestaurantsTableSeeder.php
@@ -16,7 +16,7 @@ class RestaurantsTableSeeder extends Seeder
             [
                 "name" => "McDonald's",
                 "address" => "Passaggio Duomo, 2, 20123 Milano MI",
-                "img" => "upload/ImgRestaurant/LlzO7jOR7tGmDJ0nNKyDPEvBmXiXwzedMQ1x8LmG.webp",
+                "img" => "upload/ImgRestaurant/mc.webp",
                 "logo" => "https://i.pinimg.com/originals/3e/cb/a8/3ecba88bff841698cef03d71220adaf6.jpg",
                 "vat" => "1234",
                 "user_id" => "1",
@@ -24,7 +24,7 @@ class RestaurantsTableSeeder extends Seeder
             [
                 "name" => "Pizzeria Toto",
                 "address" => "Amerigo Vespucci, 2, 20032 Cormano MI",
-                "img" => "upload/ImgRestaurant/LlzO7jOR7tGmDJ0nNKyDPEvBmXiXwzedMQ1x8LmG.webp",
+                "img" => "upload/ImgRestaurant/toto.jpg",
                 "logo" => "https://i.pinimg.com/originals/3e/cb/a8/3ecba88bff841698cef03d71220adaf6.jpg",
                 "vat" => "1234",
                 "user_id" => "2",
@@ -32,7 +32,7 @@ class RestaurantsTableSeeder extends Seeder
             [
                 "name" => "KFC",
                 "address" => "Via Bombreis, 2, 20123 Napoli NA",
-                "img" => "https://wine.pambianconews.com/wp-content/uploads/sites/15/2022/02/KFC-Dalmine-drive.jpeg",
+                "img" => "upload/ImgRestaurant/kfc.jpg",
                 "logo" => "https://www.romaest.cc/fileadmin/user_upload/GLOBAL/brand_stores/logos/kfc.jpg",
                 "vat" => "1234",
                 "user_id" => "3",
@@ -40,7 +40,7 @@ class RestaurantsTableSeeder extends Seeder
             [
                 "name" => "POKESCUSE",
                 "address" => "Via Mazzin, 2, 20123 Milano MI",
-                "img" => "https://blog.giallozafferano.it/dulcisinforno/wp-content/uploads/2021/06/Poke-bowl-ricetta-7893.jpg",
+                "img" => "upload/ImgRestaurant/kfc.jpg",
                 "logo" => "https://www.romaest.cc/fileadmin/user_upload/GLOBAL/brand_stores/logos/kfc.jpg",
                 "vat" => "1234",
                 "user_id" => "4",
