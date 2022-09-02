a581552ee20f396f1ee34d9ac70da0b26ae7e1a9
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-02 17:58:52 +0200

    adding 404 page

---
+++ resources/js/pages/Page404.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>Page not found</h1>
+    <img src="../components/imgs/purple-logo.jpeg" alt="logo">
+    <p>
+        <router-link :to="{name: 'home'}">Torna alla Home</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Page404'
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 500px;
+}
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -4,6 +4,8 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 import Home from "./pages/Home";
+import Page404 from "./pages/Page404";
+
 const router = new VueRouter({
     mode: "history", //permette la navigazione all'indietro
     routes: [
@@ -11,6 +13,11 @@ const router = new VueRouter({
             path: "/",
             name: "home",
             component: Home
+        },
+        {   // questo link deve essere ASSOLUTAMENTE ultimo del router
+            path: "/*",
+            name: "page-404",
+            component: Page404
         },
     ]
 });
