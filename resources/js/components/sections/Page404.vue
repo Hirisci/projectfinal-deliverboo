4c123223eb376c2f86917054f71f0f581ede8bbb
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-04 12:49:46 +0200

    make 404 page

---
+++ resources/js/components/sections/Page404.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <h3>Page not found</h3>
+        <img src="../imgs/c404.png" alt="404">
+        <div>
+            <router-link :to="{ name: 'home' }">Return to Home</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Page404',
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    text-align: center;
+    padding: 50px;
+}
+img {
+    width: 500px;
+}
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -5,6 +5,8 @@ Vue.use(VueRouter);
 
 import Home from "./pages/Home";
 import RestaurantSection from "./pages/RestaurantSection";
+import Page404 from "./components/sections/Page404";
+
 const router = new VueRouter({
     mode: "history", //permette la navigazione all'indietro
     routes: [
@@ -17,6 +19,11 @@ const router = new VueRouter({
             path: "/restaurant",
             name: "restaurant",
             component: RestaurantSection
+        },
+        {   // rotta 404 sempre per ultima
+            path: "/*",
+            name: "page-404",
+            component: Page404
         },
     ]
 });
