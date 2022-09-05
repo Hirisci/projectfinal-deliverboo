b477ff2e8ea183386609c48ea8a9e7490dd4d3e0
Giaki22 <giaki22@hotmail.com>
2022-09-05 16:35:29 +0200

    + Request

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OrderRequest;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
 
@@ -11,12 +12,36 @@ class OrderController extends Controller
     public function generate(Request $request, Gateway $gateway){
         $token = $gateway->clientToken()->generate();
         $data = [
+            'success' => true,
             'token' => $token
         ];
         return response()->json($data);
     }
 
-    public function makePayment(Request $request){
+    public function makePayment(OrderRequest $request, Gateway $gateway, $amount){
+
+        $result = $gateway->transaction()->sale([
+            'amount' => $amount,
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+
+        if($result->success){
+            $data = [
+                'success' => true,
+                'message' => "Transazione eseguita"
+            ];
+            return response()->json($data,200);
+        }else{
+            $data = [
+                'success' => false,
+                'message' => "Transazione fallita"
+            ];
+            return response()->json($data,401);
+        }
+
         return 'make-payment';
     }
 }
