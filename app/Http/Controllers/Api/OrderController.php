68c60260a3d2a76f97cdf62a7bcf251d7948b103
Giaki22 <giaki22@hotmail.com>
2022-09-05 15:42:22 +0200

    + Generated Payment Token

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Braintree\Gateway;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function generate(Request $request){
-        return 'generate';
+    public function generate(Request $request, Gateway $gateway){
+        $token = $gateway->clientToken()->generate();
+        $data = [
+            'token' => $token
+        ];
+        return response()->json($data);
     }
 
     public function makePayment(Request $request){
