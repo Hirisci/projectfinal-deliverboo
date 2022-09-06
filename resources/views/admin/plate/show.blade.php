0c325ad206f3cabf91b3b70c1d259eddfda3058a
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-09-06 12:46:47 +0200

    align texts del show

--- resources/views/admin/plate/show.blade.php
+++ resources/views/admin/plate/show.blade.php
@@ -7,19 +7,19 @@
             <h1>Visualizzazione Piatto:</h1>
         </div>
         <div class="cards-body  py-5 p-3">
-            
+
                 <div class="my-3">
                     <h4 class="mb-2" >Nome Piatto: </h4>
-                    <h3>{{$plate->name}}</h3>
+                    <h3 class="mx-2">{{$plate->name}}</h3>
                 </div>
                 <div class="mb-3">
                     <h4 class="mb-2">Descrizione Piatto: </h4>
-                    <h5>{{$plate->description}}</h5>
+                    <h5 class="mx-2">{{$plate->description}}</h5>
                 </div>
                 
                 <div class="mb-3">
                     <h4 class="mb-2">Costo Piatto: </h4>
-                    <h2>{{number_format($plate->price, 2)}}€</h2>
+                    <h2 class="mx-2">{{number_format($plate->price, 2)}}€</h2>
                 </div>
                 
 
