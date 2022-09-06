94397a86930fcc83b334a1a873b876c5039f20fe
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-06 22:59:08 +0200

    adding both button on ACartItem

--- resources/js/components/atoms/ACartItem.vue
+++ resources/js/components/atoms/ACartItem.vue
@@ -2,7 +2,8 @@
   <div class="cart-item">
     <div class="cart-item-left">
       <div class="cart-item-delete">
-        <button class="cart-item-delete-btn" @click="del">-</button>
+        <button class="cart-item add-btn">+</button>
+        <button class="cart-item delete-btn" @click="del">-</button>
       </div>
       <div class="cart-item-left-info">
         <div class="cart-item-quantity">
@@ -83,16 +84,34 @@ export default {
     .cart-item-delete {
       margin-left: -2.125rem;
       height: 100%;
-      .cart-item-delete-btn {
-        border-top-right-radius: 0;
-        border-bottom-right-radius: 0;
-        height: 100%;
+      .cart-item {
+        height: 50%;
         width: 2rem;
         color: white;
-        background-color: hsl(0, 100%, 45%);
+        background-color: green;
         &:hover {
           cursor: pointer;
         }
+      }
+      .add-btn {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+        background-color: green;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      .delete-btn {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+        background-color: hsl(0, 100%, 45%);
+        display: flex;
+        align-items: center;
+        justify-content: center;
       }
     }
   }
