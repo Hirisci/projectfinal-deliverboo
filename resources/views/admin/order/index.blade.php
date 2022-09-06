0282d3f30d12f48fec2640b9f8d57b34c356a619
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-09-06 12:44:22 +0200

    fix order inex and show plate

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -6,32 +6,37 @@
             <div class="cards-header">
                 <h1>Lista Ordini</h1>
             </div>
-            <div class="cards-body d-flex justify-content-between">
-                <table class="table table-striped">
-                    <thead>
-                        ordine n°:
-                    </thead>
-                    {{-- <a href="{{route('admin.order.show', $order->id)}}"> --}}
-                        <tr>
-                            <td>
-                                <ul>
-                                    <li>nome utente</li>
-                                    <li>indirizzo</li>
-                                    <li>totale</li>
-                                </ul>
-                            </td>
-                            <td>
-                                <ul>
-                                    <li>elenco piatti</li>
-                                </ul>
-                            </td>
-                            <td>
-                                totale
-                            </td>
-                            {{-- <button><a href="{{route('admin.order.show', $order->id)}}">visualizza ordine</a></button> --}}
-                        </tr>
-                    {{-- </a> --}}
-                </table>
+            <div class="cards-body py-5 p-3">
+                <div class="row">
+                    <div class="col-2 ">
+                        <h5>Ordine n°:</h5>
+                        2222
+                    </div>
+                    <div class="col-3">
+                        <h5 class="mb-2">Dati cliente</h5>
+                        <ul>
+                            <li>nome</li>
+                            <li>cognome</li>
+                            <li>indirizzo</li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <h5 class="mb-2">Riepilogo prodotti</h5>
+                        <ul>
+                            <li>Piatto</li>
+                            <li>Piatto</li>
+                            <li>Piatto</li>
+                        </ul>
+                    </div>
+                    <div class="col-2">
+                        <h5 class="mb-2">Data e Ora</h5>
+                        <ul>
+                            <li>Data e Ora dell'ordine</li>
+                            <li>Ora prevista</li>
+                            <li>Ora di arrivo</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
