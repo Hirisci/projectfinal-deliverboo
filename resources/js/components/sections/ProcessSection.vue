2382df0a8bc24f44362b910016a2e05df9b7126b
Levva01 <vellanifederico13@gmail.com>
2022-09-07 12:10:11 +0200

    desktop version completed

--- resources/js/components/sections/ProcessSection.vue
+++ resources/js/components/sections/ProcessSection.vue
@@ -157,4 +157,124 @@ export default {
 
     }
 
+@media only screen and (min-width: 21.875rem){
+
+    .section-process{
+
+        width: 100vw;
+        background-color: var(--secondary-purple);
+        padding: 1.25rem 0;
+
+        .container{
+
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            align-items: center;
+
+            .img-container{
+                padding-top: 1.25rem;
+
+                width: 90%;
+
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                .fattorino{
+                    width: 80%;
+                    border-radius: 1.25rem;
+                }
+
+            }
+
+            .right-side{
+                padding: 3.125rem 0;
+                display: flex;
+                width: 95%;
+                margin: 0;
+
+                &-dots{
+                    padding-right: .625rem;
+                    align-items: flex-end;
+                    display: flex;
+                    flex-direction: column;
+                    gap: .3125rem;
+                    width: 37%;
+
+                    .big-dot{
+
+                        width: 1.875rem;
+                        height: 1.875rem;
+                        border-radius: 50%;
+                        background-color: var(--tertiary-purple);
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+
+                        &-inside{
+
+                            width: .9375rem;
+                            height: .9375rem;
+                            border-radius: 50%;
+                            background-color: var(--primary-purple);
+
+                        }
+                    }
+
+                    .mini-dots-container{
+
+                        width: 1.875rem;
+                        display: flex;
+                        align-items: center;
+                        flex-direction: column;
+                        gap: .3125rem;
+
+                        .mini-dot{
+                        width: .3125rem;
+                        height: .3125rem;
+                        border-radius: 50%;
+                        background-color: var(--tertiary-purple);
+                        }
+
+                    }
+
+                }
+
+                &-words{
+                    display: flex;
+                    flex-direction: column;
+                    gap: 2.5rem;
+                    width: 50%;
+                }
+            }
+
+        }
+
+    }
+}
+
+@media only screen and (min-width: 768px){
+    .section-process{
+
+        padding-bottom: 2.5rem;
+
+        .container{
+            flex-direction: row;
+
+            .img-container{
+                width: 100%;
+
+                .fattorino{
+                    width: 60%;
+                }
+
+            }
+
+
+        }
+
+    }
+}
+
 </style>
