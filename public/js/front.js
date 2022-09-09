78e42667790818910e6146ab629d42506c470bbf
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-09-09 15:19:36 +0200

    aggiunti social e capibara nel footer

--- public/js/front.js
+++ public/js/front.js
@@ -2947,14 +2947,106 @@ var render = function render() {
   var _vm = this,
       _c = _vm._self._c;
 
-  return _vm._m(0);
+  return _c("footer", [_vm._m(0), _vm._v(" "), _vm._m(1), _vm._v(" "), _c("div", {
+    staticClass: "row"
+  }, [_c("div", {
+    staticClass: "col d-flex justify-content-center align-items-center"
+  }, [_c("div", {
+    staticClass: "mx-2"
+  }, [_c("svg", {
+    attrs: {
+      width: "70",
+      height: "70",
+      viewBox: "0 0 70 70",
+      fill: "none",
+      xmlns: "http://www.w3.org/2000/svg"
+    }
+  }, [_c("g", {
+    attrs: {
+      "clip-path": "url(#clip0_353_22)"
+    }
+  }, [_c("path", {
+    attrs: {
+      d: "M61.25 14.5833V55.4167C61.25 58.6383 58.6383 61.25 55.4167 61.25H44.4792C43.6738 61.25 43.0208 60.5971 43.0208 59.7917V42.2917H49.8458C50.5389 42.2918 51.1364 41.8041 51.275 41.125L52.325 35.875C52.3683 35.6629 52.3152 35.4425 52.18 35.2735C52.0448 35.1045 51.8414 35.0043 51.625 35H43.0208V25.5208C43.0208 25.1181 43.3473 24.7917 43.75 24.7917H51.0417C51.8471 24.7917 52.5 24.1387 52.5 23.3333V18.9583C52.5 18.1529 51.8471 17.5 51.0417 17.5H43.75C38.9175 17.5 35 21.4175 35 26.25V35H31.4125C30.6071 35 29.9542 35.6529 29.9542 36.4583V40.8333C29.9542 41.6387 30.6071 42.2917 31.4125 42.2917H35V59.7917C35 60.5971 34.3471 61.25 33.5417 61.25H14.5833C11.3617 61.25 8.75 58.6383 8.75 55.4167V14.5833C8.75 11.3617 11.3617 8.75 14.5833 8.75H55.4167C58.6383 8.75 61.25 11.3617 61.25 14.5833Z",
+      fill: "#9667E0"
+    }
+  })]), _vm._v(" "), _c("defs", [_c("clipPath", {
+    attrs: {
+      id: "clip0_353_22"
+    }
+  }, [_c("rect", {
+    attrs: {
+      width: "70",
+      height: "70",
+      fill: "white"
+    }
+  })])])])]), _vm._v(" "), _c("div", {
+    staticClass: "mx-2"
+  }, [_c("svg", {
+    attrs: {
+      width: "53",
+      height: "53",
+      viewBox: "0 0 53 53",
+      fill: "none",
+      xmlns: "http://www.w3.org/2000/svg"
+    }
+  }, [_c("path", {
+    attrs: {
+      d: "M37.9167 0H14.5833C6.52918 0 0 6.52918 0 14.5833V37.9167C0 45.9708 6.52918 52.5 14.5833 52.5H37.9167C45.9708 52.5 52.5 45.9708 52.5 37.9167V14.5833C52.5 6.52918 45.9708 0 37.9167 0ZM47.3958 37.9167C47.3798 43.1452 43.1452 47.3798 37.9167 47.3958H14.5833C9.35478 47.3798 5.12018 43.1452 5.10417 37.9167V14.5833C5.12018 9.35478 9.35478 5.12018 14.5833 5.10417H37.9167C43.1452 5.12018 47.3798 9.35478 47.3958 14.5833V37.9167ZM43.0208 12.3958C43.0208 14.0067 41.715 15.3125 40.1042 15.3125C38.4933 15.3125 37.1875 14.0067 37.1875 12.3958C37.1875 10.785 38.4933 9.47917 40.1042 9.47917C41.715 9.47917 43.0208 10.785 43.0208 12.3958ZM26.25 13.125C19.0013 13.125 13.125 19.0013 13.125 26.25C13.125 33.4987 19.0013 39.375 26.25 39.375C33.4987 39.375 39.375 33.4987 39.375 26.25C39.3828 22.7667 38.0025 19.4238 35.5394 16.9606C33.0763 14.4975 29.7333 13.1172 26.25 13.125ZM26.25 34.2708C21.8202 34.2708 18.2292 30.6798 18.2292 26.25C18.2292 21.8202 21.8202 18.2292 26.25 18.2292C30.6798 18.2292 34.2708 21.8202 34.2708 26.25C34.2708 30.6798 30.6798 34.2708 26.25 34.2708Z",
+      fill: "#9667E0"
+    }
+  })])]), _vm._v(" "), _c("div", {
+    staticClass: "mx-2"
+  }, [_c("svg", {
+    attrs: {
+      width: "70",
+      height: "70",
+      viewBox: "0 0 70 70",
+      fill: "none",
+      xmlns: "http://www.w3.org/2000/svg"
+    }
+  }, [_c("path", {
+    attrs: {
+      d: "M64.0796 18.8528C62.5744 20.8605 60.7551 22.6118 58.6918 24.0391C58.6918 24.5636 58.6918 25.088 58.6918 25.6416C58.7084 35.1657 54.904 44.2982 48.1317 50.9915C41.3594 57.6849 32.1856 61.3794 22.6669 61.2466C17.1639 61.265 11.7314 60.0085 6.79492 57.5754C6.52875 57.4592 6.35707 57.1958 6.35808 56.9053V56.5848C6.35808 56.1664 6.69709 55.8272 7.11527 55.8272C12.5246 55.6489 17.7409 53.7734 22.0262 50.4661C17.13 50.3675 12.7247 47.467 10.6974 43.0071C10.595 42.7635 10.6269 42.4841 10.7815 42.2698C10.9362 42.0556 11.1912 41.9375 11.4546 41.9582C12.9426 42.1077 14.4455 41.9692 15.8812 41.5502C10.4763 40.4283 6.41509 35.9412 5.83387 30.4492C5.81321 30.1857 5.93128 29.9305 6.14541 29.7758C6.35955 29.6211 6.6388 29.5892 6.88229 29.6916C8.33274 30.3316 9.8985 30.6687 11.4837 30.6823C6.74769 27.5738 4.70204 21.6613 6.50369 16.2888C6.68967 15.7667 7.13647 15.381 7.6799 15.2735C8.22334 15.1659 8.78324 15.3524 9.15387 15.7643C15.5448 22.5662 24.3261 26.6194 33.6462 27.0693C33.4075 26.1167 33.29 25.1377 33.2967 24.1557C33.3839 19.0061 36.5701 14.4188 41.3637 12.5413C46.1573 10.6639 51.6096 11.8678 55.168 15.5895C57.5936 15.1274 59.9386 14.3126 62.1283 13.1711C62.2887 13.071 62.4921 13.071 62.6525 13.1711C62.7526 13.3316 62.7526 13.5351 62.6525 13.6956C61.5919 16.1244 59.8001 18.1616 57.5269 19.5229C59.5175 19.2921 61.473 18.8225 63.3515 18.1244C63.5097 18.0167 63.7175 18.0167 63.8757 18.1244C64.0082 18.185 64.1073 18.3009 64.1465 18.4413C64.1858 18.5816 64.1613 18.7321 64.0796 18.8528Z",
+      fill: "#9667E0"
+    }
+  })])]), _vm._v(" "), _c("div", {
+    staticClass: "mx-2"
+  }, [_c("svg", {
+    attrs: {
+      width: "70",
+      height: "70",
+      viewBox: "0 0 70 70",
+      fill: "none",
+      xmlns: "http://www.w3.org/2000/svg"
+    }
+  }, [_c("g", {
+    attrs: {
+      "clip-path": "url(#clip0_353_26)"
+    }
+  }, [_c("path", {
+    attrs: {
+      d: "M55.4167 8.75H14.5833C11.3617 8.75 8.75 11.3617 8.75 14.5833V55.4167C8.75 58.6383 11.3617 61.25 14.5833 61.25H55.4167C58.6383 61.25 61.25 58.6383 61.25 55.4167V14.5833C61.25 11.3617 58.6383 8.75 55.4167 8.75ZM24.7917 51.0417C24.7917 51.8471 24.1387 52.5 23.3333 52.5H18.9583C18.1529 52.5 17.5 51.8471 17.5 51.0417V30.625C17.5 29.8196 18.1529 29.1667 18.9583 29.1667H23.3333C24.1387 29.1667 24.7917 29.8196 24.7917 30.625V51.0417ZM21.1458 26.25C18.7296 26.25 16.7708 24.2912 16.7708 21.875C16.7708 19.4588 18.7296 17.5 21.1458 17.5C23.5621 17.5 25.5208 19.4588 25.5208 21.875C25.5208 24.2912 23.5621 26.25 21.1458 26.25ZM52.5 51.0417C52.5 51.8471 51.8471 52.5 51.0417 52.5H46.6667C45.8613 52.5 45.2083 51.8471 45.2083 51.0417V40.1042C45.2083 37.6879 43.2496 35.7292 40.8333 35.7292C38.4171 35.7292 36.4583 37.6879 36.4583 40.1042V51.0417C36.4583 51.8471 35.8054 52.5 35 52.5H30.625C29.8196 52.5 29.1667 51.8471 29.1667 51.0417V30.625C29.1667 29.8196 29.8196 29.1667 30.625 29.1667H35C35.8054 29.1667 36.4583 29.8196 36.4583 30.625V32.375C38.2324 29.6754 41.3497 28.1728 44.5667 28.4667C49.168 29.034 52.5947 32.9898 52.5 37.625V51.0417Z",
+      fill: "#9667E0"
+    }
+  })]), _vm._v(" "), _c("defs", [_c("clipPath", {
+    attrs: {
+      id: "clip0_353_26"
+    }
+  }, [_c("rect", {
+    attrs: {
+      width: "70",
+      height: "70",
+      fill: "white"
+    }
+  })])])])])])]), _vm._v(" "), _vm._m(2)]);
 };
 
 var staticRenderFns = [function () {
   var _vm = this,
       _c = _vm._self._c;
 
-  return _c("footer", [_c("div", {
+  return _c("div", {
     staticClass: "footer-top container p-0"
   }, [_c("div", {
     staticClass: "row d-flex"
@@ -3054,9 +3146,28 @@ var staticRenderFns = [function () {
       src: __webpack_require__(/*! ../imgs/google-play.png */ "./resources/js/components/imgs/google-play.png"),
       alt: "Google Play"
     }
-  })])])])])]), _vm._v(" "), _c("div", {
+  })])])])])]);
+}, function () {
+  var _vm = this,
+      _c = _vm._self._c;
+
+  return _c("div", {
+    staticClass: "row mt-4"
+  }, [_c("div", {
+    staticClass: "col text-center"
+  }, [_c("h5", [_vm._v("Vieni a trovarci anche nei nostri social")])])]);
+}, function () {
+  var _vm = this,
+      _c = _vm._self._c;
+
+  return _c("div", {
     staticClass: "footer-bottom d-flex justify-content-center align-items-center mt-5 p-2"
-  }, [_c("p", [_vm._v("Copyright © 2022 Team2Boolean. All Rights Reserved")])])]);
+  }, [_c("p", [_vm._v("Copyright © 2022 Team2Boolean. All Rights Reserved")]), _vm._v(" "), _c("img", {
+    attrs: {
+      src: __webpack_require__(/*! ../imgs/logo-mobile-white.png */ "./resources/js/components/imgs/logo-mobile-white.png"),
+      alt: "loghetto carino"
+    }
+  })]);
 }];
 render._withStripped = true;
 
@@ -4238,7 +4349,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "footer[data-v-cc917e4c] {\n  background-color: var(--secondary-purple);\n}\nfooter .footer-top-card[data-v-cc917e4c] {\n  background-color: var(--tertiary-purple-opacity);\n  border-radius: 1.25rem;\n  height: 100%;\n}\nfooter .app[data-v-cc917e4c] {\n  height: 3rem;\n  width: 10rem;\n}\nfooter h4[data-v-cc917e4c] {\n  margin-bottom: 1rem;\n  color: var(--primary-purple);\n  font-weight: bold;\n}\nfooter a[data-v-cc917e4c] {\n  text-decoration: none;\n}\nfooter li[data-v-cc917e4c] {\n  margin-bottom: 0.3rem;\n}\nfooter .footer-bottom[data-v-cc917e4c] {\n  background-color: var(--primary-purple);\n  color: var(--tertiary-purple);\n}", ""]);
+exports.push([module.i, "footer[data-v-cc917e4c] {\n  background-color: var(--secondary-purple);\n}\nfooter .footer-top-card[data-v-cc917e4c] {\n  background-color: var(--tertiary-purple-opacity);\n  border-radius: 1.25rem;\n  height: 100%;\n}\nfooter .app[data-v-cc917e4c] {\n  height: 3rem;\n  width: 10rem;\n}\nfooter h4[data-v-cc917e4c], footer h5[data-v-cc917e4c] {\n  margin-bottom: 1rem;\n  color: var(--primary-purple);\n  font-weight: bold;\n}\nfooter a[data-v-cc917e4c] {\n  text-decoration: none;\n}\nfooter li[data-v-cc917e4c] {\n  margin-bottom: 0.3rem;\n}\nfooter .footer-bottom[data-v-cc917e4c] {\n  background-color: var(--primary-purple);\n  color: var(--tertiary-purple);\n}\nfooter .footer-bottom img[data-v-cc917e4c] {\n  height: 1rem;\n  margin-left: 0.3125rem;\n}", ""]);
 
 // exports
 
@@ -21958,6 +22069,17 @@ module.exports = "/images/google-play.png?1b978d37810274f3e9fa196c527a9621";
 /***/ (function(module, exports) {
 
 module.exports = "/images/hamburger.png?8df3d89112d77bf236de3709c958c7a2";
+
+/***/ }),
+
+/***/ "./resources/js/components/imgs/logo-mobile-white.png":
+/*!************************************************************!*\
+  !*** ./resources/js/components/imgs/logo-mobile-white.png ***!
+  \************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/images/logo-mobile-white.png?aa1ccea7804b0adeeeb80d384c7a1172";
 
 /***/ }),
 
