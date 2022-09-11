1b843da36b51198a13622262777e6a87da87013d
Hirisci <www.alan92@icloud.com>
2022-09-11 20:27:37 +0200

    fix- prendere la pivo con la relazione per ordin

--- app/Order.php
+++ app/Order.php
@@ -9,7 +9,7 @@ class Order extends Model
 
     public function plate()
     {
-        return $this->belongsToMany('App\Plate');
+        return $this->belongsToMany('App\Plate')->withPivot('quantity');
     }
 }
 
