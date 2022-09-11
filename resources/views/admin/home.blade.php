7628248120ef94f500bbb585af3b84b802771fe5
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-09-11 16:51:24 +0200

    finitura dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,36 +1,42 @@
 @extends('layouts.dashboard')
 
 @section('menu')
-    <div class="container restaurant">
+    <div class="container plate">
         <div class="cards">
             <div class="cards-header">
                 <h1>La tua Dashboard</h1>
             </div>
-            <div class="cards-body">
-            <div class="cards-info">    
+        <div class="cards-body">
+            <div class="cards-display">
+                <div class="cards-info">    
                 <h3 class="mb-3">Statistiche vendita:</h3>
-                <div class="row">
-                    <div class="col-12">
-                        {!! $chart->container() !!}
-                        
-                        {!! $chart->script() !!}
+                    <div class="row">
+                        <div class="col-12">
+                            <h4>I tuoi ordini questo mese:</h4>
+                            {!! $chart->container() !!}
+                            
+                            {!! $chart->script() !!}
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        {!! $totalChart->container() !!}
-
-                        {!! $totalChart->script() !!}
+                    <div class="row mt-4">
+                        <div class="col-6">
+                            <h4>Il tuo scontrino mensile</h4>
+                            {!! $totalChart->container() !!}
+    
+                            {!! $totalChart->script() !!}
+                        </div>
+    
+                        <div class="col-6">
+                            <h4>I tuoi piatti più venduti</h4>
+                            {!! $avgChart->container() !!}
+    
+                            {!! $avgChart->script() !!}
+                        </div>
                     </div>
 
-                    <div class="col-6">
-                        {!! $avgChart->container() !!}
-
-                        {!! $avgChart->script() !!}
-                    </div>
-                </div>
-                
+                </div>    
         </div>
 
+        </div>
     </div>
 @endsection
